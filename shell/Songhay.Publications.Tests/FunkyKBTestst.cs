92caf5402994256c3b3b35072f948fe94f96253f
Bryan Wilhite <rasx@kintespace.com>
2019-05-27 21:31:24 -0700

    :tractor: :sparkles: :umbrella: issue #7

---
+++ shell/Songhay.Publications.Tests/FunkyKBTestst.cs
@@ -0,0 +1,15 @@
+using Songhay.Tests;
+using System;
+using System.IO;
+using Xunit;
+
+namespace Songhay.Publications.Tests
+{
+    public class FunkyKBTests
+    {
+        [Theory, ProjectFileData("../../../../../_data/kb/entries/kb-19901868.json")]
+        public void ShouldLoadEntry(FileInfo entryInfo)
+        {
+        }
+    }
+}
