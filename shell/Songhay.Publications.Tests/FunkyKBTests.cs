e8fcdd5f91a9527fe1c19f82d048009d560a6449
Bryan Wilhite <bryan.wilhite@gmail.com>
2019-06-06 17:35:48 -0700

    :microscope: issue #7

--- shell/Songhay.Publications.Tests/FunkyKBTests.cs
+++ shell/Songhay.Publications.Tests/FunkyKBTests.cs
@@ -14,10 +14,15 @@ namespace Songhay.Publications.Tests
             this._testOutputHelper = helper;
         }
 
-        [Theory, ProjectFileData(typeof(FunkyKBTests), "../../../../../_data/kb/entries/kb-19901868.json")]
-        public void ShouldLoadEntry(FileInfo entryInfo)
+        [Theory, ProjectFileData(typeof(FunkyKBTests),
+            "../../../../../_data/kb/entries/kb-19901868.json",
+            "../../../../../posts/kb-19901868.md")]
+        public void ShouldLoadEntry(FileInfo entryInfo, FileInfo mdInfo)
         {
             var jO = JObject.Parse(File.ReadAllText(entryInfo.FullName));
+            var md = string.Empty;
+
+            File.WriteAllText(mdInfo.FullName, md);
         }
 
         ITestOutputHelper _testOutputHelper;
